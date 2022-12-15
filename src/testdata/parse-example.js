2bc7b4ae9b0f123ab1e80e064b7872a3e938e9fb
Peter Squicciarini <stripedpajamas273@gmail.com>
2022-12-14 20:13:17 -0800

    Add test data

---
+++ src/testdata/parse-example.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+const readpuz = require('@confuzzle/readpuz').readpuz;
+
+const puz = readpuz(
+    fs.readFileSync(
+        path.join(
+            process.cwd(),
+            'src',
+            'testdata',
+            'Cryptic229.puz'
+        )));
+
+console.log(puz);
