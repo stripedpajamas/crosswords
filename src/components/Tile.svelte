8afed8a1ebd98a5cbc1037651097a5af3f42591b
Peter Squicciarini <stripedpajamas273@gmail.com>
2022-12-19 12:19:19 -0800

    Basic static puzzle rendering

---
+++ src/components/Tile.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  export let value: string;
+</script>
+
+<div class:filler={value === '.'}>
+  <p>{value}</p>
+</div>
+
+<style>
+  div {
+    border: 1px solid black;
+    display: grid;
+    justify-content: center;
+    align-content: center;
+    text-align: center;
+    width: 100%;
+    font-size: 24px;
+  }
+
+  .filler {
+    background-color: black;
+  }
+</style>
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="svelte" />
+
+export interface ParsedPuz {
+    solution: string;
+    state: string;
+    title: string;
+    author: string;
+    copyright: string;
+    width: number;
+    height: number;
+    clues: string[];
+}
