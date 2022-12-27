428e29498083cf79bc615a58c312380674ee1a59
Peter Squicciarini <stripedpajamas273@gmail.com>
2022-12-26 20:36:32 -0800

    Add logging

--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -1,39 +1,48 @@
 import { error } from '@sveltejs/kit';
+import { Logtail } from '@logtail/browser';
 import { readpuz } from '@confuzzle/readpuz';
 import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
 import { R2_ENDPOINT, ACCESS_KEY, SECRET_KEY } from '$env/static/private';
 
+const logtail = new Logtail('1AUqbfSc2uLekZ7MuLM8qwsE');
 const r2 = new S3Client({
-  region: 'auto',
-  endpoint: R2_ENDPOINT,
-  credentials: {
-    accessKeyId: ACCESS_KEY,
-    secretAccessKey: SECRET_KEY,
-  },
+	region: 'auto',
+	endpoint: R2_ENDPOINT,
+	credentials: {
+		accessKeyId: ACCESS_KEY,
+		secretAccessKey: SECRET_KEY
+	}
 });
 
 export async function load({ params }) {
 	if (!params.slug) {
+		logtail.info("no slug, can't load puz file", params);
 		return error(404);
 	}
 
-  let res;
-  try {
+	let res;
+	try {
 		res = await r2.send(
 			new GetObjectCommand({
 				Bucket: 'crosswords',
 				Key: params.slug
 			})
 		);
-	} catch (e) {
-		console.error(e);
+	} catch (e: any) {
+		logtail.error('error getting object from r2', e);
 		return error(404);
 	}
-  const puzBytes = await res.Body?.transformToByteArray();
-  const puz = readpuz(puzBytes);
 
-  // Some binary stuff we don't care about
-  delete puz.sections;
+	let puz;
+	try {
+		const puzBytes = await res.Body?.transformToByteArray();
+		puz = readpuz(puzBytes);
+	} catch (e: any) {
+		logtail.error('error parsing puz bytes', e);
+	}
+
+	// Some binary stuff we don't care about
+	delete puz.sections;
 
 	return { puz };
 }
